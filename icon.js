a1d7bc48d6fe6f68935b515112fcdc5ed89f3b36
porgodjabir1-code <porgodjabir1@gmail.com>
2026-05-11 12:17:11 +0000

    icon.js

--- icon.js
+++ icon.js
@@ -1,7 +1,7 @@
 const appIcon = {
   name: "DEV NOVA CREATOR",
-  icon: "https://files.catbox.moe/d6q2ze.jpeg",
-  size: "192x192",
+  icon: "https://files.catbox.moe/npn9x9.jpeg",
+  size: "512x512",
   type: "image/jpeg",
   themeColor: "#1a6fc4",
   backgroundColor: "#050d1a"
